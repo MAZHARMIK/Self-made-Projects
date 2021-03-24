52afd160b1fbb6def3b6078a7714b50951fe0a31
MAZHAR <iiestshibpur605@gmail.com>
2021-03-24 15:56:51 +0530

    Progressing slowly

--- Tcl-Source-Spider/source_spider.tcl
+++ Tcl-Source-Spider/source_spider.tcl
@@ -5,6 +5,15 @@ set paths_map(foo) foo;
 set file_map(foo) foo;
 set a 1;
 
+#To avoid colission of same file names in a directory
+proc removeCollission {file_name counter} {
+    set len [string length $file_name];
+
+    #Body: coming soon....
+
+return $file_name;
+}
+
 proc putargs args {
     global paths_map;
 
